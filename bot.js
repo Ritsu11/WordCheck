59be8928b31314374dece762ffe70767818dd081
Ishimigne_win10 <garethbale3113@gmail.com>
2021-06-11 10:24:25 +0900

    create emoji

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 // Discord Clientのインスタンス作成
 const client = new Discord.Client();
 // トークンの用意
-const token = "ODQ2NTc2Njc2MTU4MTc3MzAw.YKxh-A.RSlTuGhanAUGd4ZjM4iSkvCNnC8";
+const token = "ODUyNzE2Mjk4ODk4ODk4OTQ1.YMK38A.82uwD0WhqHqiqemd2ovT92CRlNg";
 
 //処理待ち
 client.on("ready", () => {
@@ -27,6 +27,42 @@ client.on("message", (message) => {
     //   .catch(console.error);
     // return;
 
+    //テキストに対してリアクションで対応
+    message.react("😇").catch(console.error);
+  }
+
+  if (message.content === "あ") {
+    //テキストに対してテキストで対応
+    // let channel = message.channel;
+    // let author = message.author.username;
+    // let reply_text = `😡`;
+
+    // message
+    //   .reply(reply_text)
+    //   .then((message) => console.log(`Sent message: ${reply_text}`))
+    //   .catch(console.error);
+    // return;
+
+    //テキストに対してリアクションで対応
+    message.react("😇").catch(console.error);
+  }
+
+  if (message.content === "かわいい") {
+    //テキストに対してリアクションで対応
+    message.react("").catch(console.error);
+  }
+
+  if (message.content === "い") {
+    //テキストに対してリアクションで対応
+    message.react("😇").catch(console.error);
+  }
+
+  if (message.content === "い") {
+    //テキストに対してリアクションで対応
+    message.react("😇").catch(console.error);
+  }
+
+  if (message.content === "い") {
     //テキストに対してリアクションで対応
     message.react("😇").catch(console.error);
   }
