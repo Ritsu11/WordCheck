36d53f4e73c09b2eb1e2c797fc1d7d6f69588286
Ishimigne_win10 <garethbale3113@gmail.com>
2021-06-15 11:53:26 +0900

    不適切な言葉追加

--- bot.js
+++ bot.js
@@ -6,19 +6,22 @@ const fs = require("fs");
 // Discord Clientのインスタンス作成
 const client = new Discord.Client();
 // トークンの用意
-const token = "";
+const token = "ODUyNzE2Mjk4ODk4ODk4OTQ1.YMK38A.sGH81b67SOzWxH96lhAl071qt6U";
 
 //テキストファイル読み込み
 let textAnger = fs.readFileSync("./emotions/oko.txt", "utf8");
 let textJoy = fs.readFileSync("./emotions/yorokobi.txt", "utf8");
+let textBad = fs.readFileSync("./emotions/Bad.txt", "utf8");
 
 //分割して配列にまとめ
 let Angers = textAnger.toString().split("\n");
 let Joys = textJoy.toString().split("\n");
+let Bads = textBad.toString().split("\n");
 
 //分割確認コンソール
 console.log(Angers);
 console.log(Joys);
+console.log(Bads);
 
 //処理待ち
 client.on("ready", () => {
@@ -41,6 +44,13 @@ client.on("message", (message) => {
   for (let Joy of Joys) {
     if (message.content.match(Joy)) {
       message.react("😆").catch(console.error);
+    }
+  }
+
+  //不適切検出
+  for (let Bad of Bads) {
+    if (message.content.match(Bad)) {
+      message.react("💩").catch(console.error);
     }
   }
 });
